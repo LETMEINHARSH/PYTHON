a41fd20b59e94db67d9328e5038495d63b67eb0b
LETMEINHARSH <harsh.r.6541@gmail.com>
2024-04-10 09:14:07 +0530

    class003

--- arithmetic operators programme.py
+++ arithmetic operators programme.py
@@ -2,8 +2,8 @@
 
 a=float(input("Enter a number="))
 b=float(input("Enter a number="))
-print("the sum is=",a+b)
+print("the sum of the number is=",a+b)
 print('the subtraction of the number is',a-b)
 print("the multiplication of the number is",a*b)
-print("the quotient of the number is",a/b)
+print("the division of the number is",a/b)
 print("the remainder of the number is",a%b)
