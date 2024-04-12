5242c63fe98121443255af43f457a8a6752af091
LETMEINHARSH <harsh.r.6541@gmail.com>
2024-04-12 15:21:24 +0530

    python

--- condition eligible for voting or not.py
+++ condition eligible for voting or not.py
@@ -1,6 +1,6 @@
 # WAP to check whether a person is eligible for voting or not.
 a=int(input("Enter the age="))
-if a>=21:
+if a>=18:
     print("You are eligible for voting")
 else:
     print("You are not eligible for voting")
--- condition leap year.py
+++ condition leap year.py
@@ -1,3 +1,6 @@
 # Create a Python program to determine whether a given year is a leap year or not. A leap year is divisible by 4, except for years that are divisible by 100 but not by 400.
-# a=int(input("Enter a year ="))
-# if a%4==0 and a%100
+a=int(input("Enter a year ="))
+if a%4==0 and a%100!=0 or a%400==0:
+    print("Given year is a leap year")
+else:
+    print("Given year is not a leap year")
