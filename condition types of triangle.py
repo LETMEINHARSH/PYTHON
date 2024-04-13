b932676df50309ec81ff33584f82ecc70a00f2fb
LETMEINHARSH <harsh.r.6541@gmail.com>
2024-04-13 15:57:58 +0530

    triangle

---
+++ condition types of triangle.py
@@ -0,0 +1,11 @@
+# accept three sides of a triangle and check whether it is equilateral,isosceles,scalene triangle.
+
+s1=float(input("Enter first side of triangle:"))
+s2=float(input("Enter second side of triangle:"))
+s3=float(input("Enter third side of triangle:"))
+if s1==s2==s3:
+    print("It is an equilateral triangle")
+elif (s1==s2 and s2!=s3) or (s1==s3 and s3!=s2) or(s2==s3 and s3!=s1):
+    print("It is an isosceles triangle")
+elif s1!=s2 and s2!=s3 and s1!=s3:
+    print("It is a scalene triangle")
