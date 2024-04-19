1ea938f0ced43fc0819ec5b86aa138ba8723a0a4
LETMEINHARSH <harsh.r.6541@gmail.com>
2024-04-19 22:24:09 +0530

    python

--- loops to print integers.py
+++ loops to print integers.py
@@ -3,12 +3,17 @@
 # For multiple of 5 print "Buzz"
 # For number divisible by 3&5 both print "FizzBuzz"
 
-for i in range (1,50):
-    if i%3==0 and i%5==0:
+a=int(input("Enter starting number:"))
+b=int(input("Enter ending number:"))
+if a<b:
+    for i in range (a,b+1):
+     if i%3==0 and i%5==0:
         print(i,"= FizzBuzz")
-    elif i%3==0:
+     elif i%3==0:
         print(i,"= Fizz")
-    elif i%5==0:
+     elif i%5==0:
         print(i,"= Buzz")
-    else:
+     else:
         print(i,"=")
+else:
+    print("Enter a valid number.")
