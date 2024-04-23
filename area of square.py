16533b85c5482de42b368357389075b72d416bd1
LETMEINHARSH <harsh.r.6541@gmail.com>
2024-04-23 18:16:27 +0530

    python

--- area of square.py
+++ area of square.py
@@ -1,2 +1,4 @@
+# WAP to calculate the area of square.
+
 a=int(input("Enter a side of the square ="))
 print("Area of the square is =",a*a)
---
+++ display last digit.py
@@ -0,0 +1,5 @@
+# WAP to display the last digit of a number.
+
+a=int(input("Enter a number="))
+c=a%10
+print("Last digit of the number is =",c)
