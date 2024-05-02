b4a7752c6be77eba2c78f1fa7cca106a3c6a90f6
LETMEINHARSH <harsh.r.6541@gmail.com>
2024-05-02 17:05:45 +0530

    while loops

---
+++ loops while sum of common digits.py
@@ -0,0 +1,5 @@
+# WAP to take two numbers as input , then check and print the sum of all the the common digits in both the number.
+
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+sum=0
